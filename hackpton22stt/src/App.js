73bbaf327484982eff4a0092fc849e9e981a0084
kenduck1 <kenko200176@gmail.com>
2022-04-02 16:11:11 -0400

    init

---
+++ hackpton22stt/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import STT from './components/STT';
+
+function App() {
+  return (
+    <div className="App">
+      <STT />
+    </div>
+  );
+}
+
+export default App;
--- speechtotext.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from google.cloud import speech
-
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "hackptonkey.json"
-speech_client = speech.SpeechClient()
-
